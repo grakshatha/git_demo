32e25aa6bc16a878b22286f8cced645a37b5a893
grakshatha <grakshtahahd@gmail.com>
2022-12-22 12:47:25 +0530

    i am add my files

---
+++ a1.py
@@ -0,0 +1,32 @@
+# Declaring different types of variable
+
+#Empty integer variable
+a1=int()
+print(a1,type(a1))
+
+a2=100
+print(a2,type(a2))
+
+#float
+f1=float()
+print(f1,type(f1))
+f2=50.123
+print(f2,type(f2))
+
+#string
+s1=str()
+s3=" "
+print(s1,type(s1))
+s2="India"
+print(s2,type(s2))
+
+#complex
+c1=complex()
+c2=6+7j
+print(type(c2))
+
+#boolean
+b1=bool()
+print(type(b1))
+b2=True
+print(type(b2))
